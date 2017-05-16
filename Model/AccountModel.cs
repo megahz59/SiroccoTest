ed335f91d4612c8456481e64ebbd5a1763d549bf
megahz59 <mikael.herz@telia.com>
2017-05-16 16:14:55 +0200

    Created late bound sample code with connection, create account, change fields in account, change task and letters and created association between account and customer

---
+++ Model/AccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Crm.Sdk.Samples.Model
+{
+    class AccountModel
+    {
+        public string AccountName { get; set; }
+        public string AdressRow1 { get; set; }
+        public string AdressRow2 { get; set; }
+        public int Revenue { get; set; }
+        public bool CreditOnHold { get; set; }
+    }
+}
